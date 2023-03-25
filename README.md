5f06097b21173e18337566f03885cc17e64cccf4
peron <peron_ap@icloud.com>
2023-03-26 00:08:23 +0800

    feat: input() as async generator

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # deno-input
+
 Fix full-width input issue with built-in `prompt()`
+
+## usage
+
+```typescript
+import { input } from "./mod.ts";
+
+const lines = input();
+
+const { value: nextLine } = await lines.next();
+// do something with `nextLine`
+
+for await (const line of lines) {
+  // do something with `line`
+}
+```
--- main.ts
+++ main.ts
@@ -1,16 +1,22 @@
 import { default as eaw } from "npm:eastasianwidth";
 
-export async function input() {
+export async function* input() {
   Deno.stdin.setRaw(true);
-  const stream = Deno.stdin.readable.pipeThrough(new TextDecoderStream());
+  const stdinStream = Deno.stdin.readable.pipeThrough(new TextDecoderStream());
   const encoder = new TextEncoder();
 
   let buffer = "";
-  for await (const chunk of stream) {
+
+  for await (const chunk of stdinStream) {
     const charCode = chunk.charCodeAt(0);
-    if (charCode === 13) break;
-    else if (charCode === 3) Deno.exit(0);
-    else if (charCode === 127) {
+    if (charCode === 3) {
+      // ctrl+c
+      Deno.exit(0);
+    } else if (charCode === 13) {
+      // enter
+      yield buffer;
+      buffer = "";
+    } else if (charCode === 127) {
       // backspace
       if (buffer.length > 0) {
         const lastChar = buffer[buffer.length - 1];
@@ -23,8 +29,4 @@ export async function input() {
       buffer += chunk;
     }
   }
-
-  Deno.stdin.setRaw(false);
-
-  return buffer;
 }
